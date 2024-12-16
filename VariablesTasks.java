cd12e55e30642afbecea770d3355f64f806f5625
HEMANTH <126463096+Hemanthhark@users.noreply.github.com>
2024-12-16 17:02:11 +0530

    Day1 part2

---
+++ VariablesTasks.java
@@ -0,0 +1,23 @@
+public class VariablesTasks{
+public static void main(String[] a)
+{
+  int num = 10;
+System.out.println("int:" +num);
+  String name = "Hemanth";
+System.out.println("String:"+name);
+  byte b = 32;
+System.out.println("byte:" +b);
+  float dot  = 5.5f;
+System.out.println("float:" +dot);
+  double d = 3.1777;
+System.out.println("double:" +d);
+  char c='C';
+System.out.println("char:" +c);
+boolean boo=true;
+System.out.println("boolean:" +boo);
+short s=347;
+System.out.println("short:" +s);
+long l=6378215454545l;
+System.out.println("long:" +l);
+}
+}
