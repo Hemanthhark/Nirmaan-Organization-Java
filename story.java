8fea9953062a17713fc4739f466b00c3feec2447
HEMANTH <126463096+Hemanthhark@users.noreply.github.com>
2024-12-27 16:05:00 +0530

    day5

---
+++ story.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class story {
+	public static void main(String[] args) {
+		
+	
+		
+	String name1="Hemanth";
+	String adjective1="Fresh";
+	String adjective2="good";
+	String noun1="Childre's";
+	String noun2="chocolates";
+	String verb1="sing";
+	String noun3="song";
+	String noun4="dogs";
+	String adjective3="confused";
+	String name2="malni";
+	String place1="Mars";
+	String noun5="lake";
+	int number=2056;
+	String noun6="Robots";		
+	
+	
+	String tale = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+ "is ruled the world.";
+	
+	System.out.println(tale);
+	
+	
+	}
+	
+}
