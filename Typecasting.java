7e6912576ae744e302f916362c97d595ee6226b3
HEMANTH <126463096+Hemanthhark@users.noreply.github.com>
2024-12-30 17:15:19 +0530

    day6 part1

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+package day6;
+
+public class Typecasting {
+public static void main(String[] args) {
+//	implicit
+//	byte b=127;
+//	int i = b;
+	
+//	Explicit
+	
+	int i1=129;
+	byte b1= (byte)i1;
+	System.out.println(b1);
+}
+}
