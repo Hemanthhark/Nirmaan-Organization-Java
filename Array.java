eed72abf0f22baea40c586d02828fe86008fc70b
HEMANTH <126463096+Hemanthhark@users.noreply.github.com>
2024-12-30 17:16:11 +0530

    day6 part3

---
+++ Array.java
@@ -0,0 +1,13 @@
+package day6;
+
+public class Array {
+	public static void main(String[] args) {
+		
+		String Fruits[]= {"Mango" ,"Apple","Grapes"};
+		
+		for (int i=0;i<Fruits.length;i++) {
+		System.out.println(Fruits[i]);
+	}
+
+}
+}
