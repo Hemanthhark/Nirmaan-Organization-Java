9e3770bc6d8cffe0f0c736ee2a8c9ee0e0548b25
HEMANTH <126463096+Hemanthhark@users.noreply.github.com>
2024-12-31 15:49:07 +0530

    day7 part2

---
+++ calculator.java
@@ -0,0 +1,44 @@
+package day7;
+
+public class calculator {
+	
+	int Fnum;
+	int Snum;
+	
+	
+	void add() {
+		System.out.println("Addition :"+(Fnum+Snum));
+	}
+		void sub() {
+			System.out.println("sub :"+(Fnum-Snum));
+	}
+			void multi() {
+				System.out.println("multi :"+(Fnum*Snum));
+		}
+				void division() {
+					System.out.println("division :"+(Fnum/Snum));
+				}
+					void modules() {
+						System.out.println("modules :"+(Fnum%Snum));
+					
+		
+	}
+					public static void main(String[] args) {
+						calculator input = new calculator();
+						calculator input2 = new calculator();
+						calculator multi = new calculator();
+						calculator input3 = new calculator();
+						calculator modules = new calculator();
+						input.Fnum=40;
+						input.Snum=20;
+						
+						
+						input.add();
+						input2.sub();
+						multi.multi();
+						input3.division();
+						modules.modules();
+						
+					}
+
+}
